de53e2d8e1967d90bfd28ac6b3cadf953bd9bf6f
Amrit Shetra <1719349@brunel.ac.uk>
2020-12-31 17:42:26 +0000

    Flask server made visible across the network.

--- server/api.py
+++ server/api.py
@@ -9,6 +9,6 @@ def home():
     return "Hello World!"
 
 
+# https://stackoverflow.com/questions/7023052/configure-flask-dev-server-to-be-visible-across-the-network
 if __name__ == '__main__':
-    app.run()
-
+    app.run(host='0.0.0.0')
