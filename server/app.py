67bcb60a6ddcaa39b949f412ecf5ab6fab31e412
Amrit Shetra <1719349@brunel.ac.uk>
2021-01-13 14:36:11 +0000

    Added Postgres container.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
 import flask
+import os
 from models import db, User
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
+
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+host = os.environ['POSTGRES_HOST']
+database = os.environ['POSTGRES_DB']
+port = os.environ['POSTGRES_PORT']
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
 
 # Specify which app we are using with SQLAlchemy
 app.app_context().push()
