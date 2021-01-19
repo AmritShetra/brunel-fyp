7e3295a6494ca06331a6579be1c57e878013c625
AmritShetra <1719349@brunel.ac.uk>
2021-01-19 23:56:37 +0000

    Added template logic screen + title screen button to change screen.

--- android/app/src/main/java/com/example/android/brunel_fyp/Title.java
+++ android/app/src/main/java/com/example/android/brunel_fyp/Title.java
@@ -1,5 +1,6 @@
 package com.example.android.brunel_fyp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -14,7 +15,11 @@ public class Title extends AppCompatActivity {
         Button logIn = findViewById(R.id.logIn);
         Button signUp = findViewById(R.id.signUp);
 
-        logIn.setOnClickListener(view -> System.out.println("logIn"));
+        logIn.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), Login.class);
+            startActivity(intent);
+            finish();
+        });
         signUp.setOnClickListener(view -> System.out.println("signUp"));
     }
 }
