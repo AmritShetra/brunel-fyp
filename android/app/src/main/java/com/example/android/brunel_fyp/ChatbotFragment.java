557ec7ae3be1bbb226b371fdde058613e90abff8
AmritShetra <1719349@brunel.ac.uk>
2021-01-30 23:40:37 +0000

    Added more options to Chatbot + using Google Flexbox layout.

--- android/app/src/main/java/com/example/android/brunel_fyp/ChatbotFragment.java
+++ android/app/src/main/java/com/example/android/brunel_fyp/ChatbotFragment.java
@@ -3,12 +3,10 @@ package com.example.android.brunel_fyp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +16,7 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.android.flexbox.FlexboxLayout;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
@@ -30,13 +29,11 @@ import cz.msebera.android.httpclient.Header;
 public class ChatbotFragment extends Fragment {
 
     ScrollView scrollView;
-    LinearLayout linearLayout;
+
     int chatbotMessage = R.layout.chatbot_message;
     int userMessage = R.layout.user_message;
 
     final int REQUEST_IMAGE_CAPTURE = 1;
-
-    AsyncHttpClient client = new AsyncHttpClient();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,30 +41,89 @@ public class ChatbotFragment extends Fragment {
         View parentHolder = inflater.inflate(R.layout.fragment_chatbot, container, false);
 
         scrollView = parentHolder.findViewById(R.id.scrollView);
-        linearLayout = parentHolder.findViewById(R.id.linearLayout);
-
-        // Show the introduction message from the chatbot to kick things off
-        addView(chatbotMessage);
-
-        // Add the user message template to the linear layout and edit the button text
-        View firstUserView = addView(userMessage);
-        Button firstUserButton = firstUserView.findViewById(R.id.userButton);
-        firstUserButton.setText(R.string.recycle_option);
-
-        firstUserButton.setOnClickListener(view -> showRecycleInstructions());
+        initialiseChatbot();
 
         return parentHolder;
     }
 
+    private void initialiseChatbot() {
+        // Show the introduction message from the chatbot to kick things off
+        addView(chatbotMessage);
+
+        // Add the user message to the screen, show the two buttons
+        View userOptions = addView(userMessage);
+        FlexboxLayout flowLayout = userOptions.findViewById(R.id.buttonContainer);
+
+        Button recycleButton = createButton(
+                getString(R.string.recycle_option)
+        );
+        flowLayout.addView(recycleButton);
+
+        Button otherButton = createButton(
+                getString(R.string.other_option)
+        );
+        flowLayout.addView(otherButton);
+
+        recycleButton.setOnClickListener(view -> showRecycleInstructions());
+        otherButton.setOnClickListener(view -> showOtherOptions());
+
+    }
+
     // Add the message view to the linear layout and scroll down
     private View addView (int viewLayout){
+        LinearLayout linearLayout = scrollView.findViewById(R.id.linearLayout);
         View newView = getLayoutInflater().inflate(viewLayout, linearLayout, false);
         linearLayout.addView(newView);
         scrollView.post(() -> scrollView.fullScroll(ScrollView.FOCUS_DOWN));
         return newView;
     }
 
-    // Make the chatbot's response visible and edit the text appropriately
+    // Create buttons that the user can press
+    private Button createButton(String text) {
+        Button button = new Button(getContext());
+        button.setText(text);
+        button.setBackgroundResource(R.drawable.user_message_shape);
+        button.setPadding(30, -0, 30, -0);
+        button.setAllCaps(false);
+        return button;
+    }
+
+    // Shows dialogue, followed by two choices for the user
+    private void showOtherOptions() {
+        View chatbotResponse = addView(chatbotMessage);
+        TextView chatbotMessage = chatbotResponse.findViewById(R.id.messageText);
+        chatbotMessage.setText(R.string.other_option_response);
+
+        View userOptions = addView(userMessage);
+        FlexboxLayout flowLayout = userOptions.findViewById(R.id.buttonContainer);
+
+        Button optionOneButton = createButton(
+                getString(R.string.other_option_1)
+        );
+        flowLayout.addView(optionOneButton);
+
+        Button optionTwoButton = createButton(
+                getString(R.string.other_option_2)
+        );
+        flowLayout.addView(optionTwoButton);
+
+        optionOneButton.setOnClickListener(view -> optionResponse(1));
+        optionTwoButton.setOnClickListener(view -> optionResponse(2));
+
+    }
+
+    // Depending on option, return a response from the chatbot
+    private void optionResponse(int option) {
+        View chatbotResponse = addView(chatbotMessage);
+        TextView chatbotMessage = chatbotResponse.findViewById(R.id.messageText);
+
+        if (option == 1)
+            chatbotMessage.setText(R.string.other_option_1_response);
+        else
+            chatbotMessage.setText(R.string.other_option_2_response);
+    }
+
+    // If the user wants to recycle an item, show them appropriate dialogue + a choice
     private void showRecycleInstructions() {
         View chatbotResponse = addView(chatbotMessage);
         TextView chatbotMessage = chatbotResponse.findViewById(R.id.messageText);
@@ -79,10 +135,14 @@ public class ChatbotFragment extends Fragment {
     // Display the user's "take photo" button
     private void showUserButton() {
         View userResponse = addView(userMessage);
-        TextView userButton = userResponse.findViewById(R.id.userButton);
-        userButton.setText(R.string.take_photo);
+        FlexboxLayout flowLayout = userResponse.findViewById(R.id.buttonContainer);
 
-        userButton.setOnClickListener(view -> launchCamera());
+        Button photoButton = createButton(
+                getString(R.string.take_photo)
+        );
+        flowLayout.addView(photoButton);
+
+        photoButton.setOnClickListener(view -> launchCamera());
     }
 
     private void launchCamera() {
@@ -96,10 +156,13 @@ public class ChatbotFragment extends Fragment {
             // Get the photo
             Bitmap photo = (Bitmap) data.getExtras().get("data");
 
-            // Display the photo
+            // Display the photo in
             ImageView imageView = new ImageView(getContext());
             imageView.setImageBitmap(photo);
-            linearLayout.addView(imageView);
+
+            View userResponse = addView(userMessage);
+            FlexboxLayout flowLayout = userResponse.findViewById(R.id.buttonContainer);
+            flowLayout.addView(imageView);
 
             sendPhoto(photo);
         }
@@ -126,6 +189,7 @@ public class ChatbotFragment extends Fragment {
         View chatbotResponse = addView(chatbotMessage);
         TextView response = chatbotResponse.findViewById(R.id.messageText);
 
+        AsyncHttpClient client = new AsyncHttpClient();
         client.post(url, params, new TextHttpResponseHandler(){
             @Override
             public void onStart(){
