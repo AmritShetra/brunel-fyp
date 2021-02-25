ad629db625e0357dd2138ac17f29161b3d62bf01
AmritShetra <1719349@brunel.ac.uk>
2021-02-25 23:44:49 +0000

    Cleaned up most of ProfileEdit Activity code.

--- android/app/src/main/java/com/example/android/brunel_fyp/ProfileEditActivity.java
+++ android/app/src/main/java/com/example/android/brunel_fyp/ProfileEditActivity.java
@@ -16,43 +16,51 @@ import android.widget.TextView;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 
 import cz.msebera.android.httpclient.Header;
 import cz.msebera.android.httpclient.entity.StringEntity;
 
 public class ProfileEditActivity extends AppCompatActivity {
 
-    EditText usernameField, passwordField, firstNameField, lastNameField, emailField;
-    ProgressBar progressBar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_edit);
 
-        usernameField = findViewById(R.id.username);
-        passwordField = findViewById(R.id.password);
-        firstNameField = findViewById(R.id.firstName);
-        lastNameField = findViewById(R.id.lastName);
-        emailField = findViewById(R.id.email);
-
-        progressBar = findViewById(R.id.progressBar);
+        EditText usernameField = findViewById(R.id.username);
+        EditText passwordField = findViewById(R.id.password);
+        EditText firstNameField = findViewById(R.id.firstName);
+        EditText lastNameField = findViewById(R.id.lastName);
+        EditText emailField = findViewById(R.id.email);
 
         // Load profile data that was retrieved from the API in the previous screen
-        setUpProfile();
+        usernameField.setText(User.getUsername(getApplicationContext()));
+        passwordField.setText(User.getPassword(getApplicationContext()));
+        // Data was passed when making a new Intent, as we left the Profile screen
+        Bundle extras = getIntent().getExtras();
+        firstNameField.setText(extras.getString("first_name"));
+        lastNameField.setText(extras.getString("last_name"));
+        emailField.setText(extras.getString("email"));
 
         ImageView cancel = findViewById(R.id.cancel);
         cancel.setOnClickListener(view -> finish());
 
         ImageView saveChanges = findViewById(R.id.saveChanges);
         saveChanges.setOnClickListener(view -> {
-            if (validateInputs()) {
+            HashMap<String, String> details = new HashMap<>();
+            details.put("username", usernameField.getText().toString());
+            details.put("password", passwordField.getText().toString());
+            details.put("email", emailField.getText().toString());
+            details.put("first_name", firstNameField.getText().toString());
+            details.put("last_name", lastNameField.getText().toString());
+
+            if (validateInputs(details)) {
                 try {
-                    updateProfile();
+                    updateProfile(details);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -70,47 +78,35 @@ public class ProfileEditActivity extends AppCompatActivity {
         });
     }
 
-    private void setUpProfile() {
-        usernameField.setText(User.getUsername(getApplicationContext()));
-        passwordField.setText(User.getPassword(getApplicationContext()));
-
-        // Data was passed when making a new Intent, as we left the Profile screen
-        Bundle extras = getIntent().getExtras();
-        firstNameField.setText(extras.getString("first_name"));
-        lastNameField.setText(extras.getString("last_name"));
-        emailField.setText(extras.getString("email"));
-    }
-
-    private boolean validateInputs() {
+    private boolean validateInputs(HashMap<String, String> details) {
         boolean valid = true;
         String text = "";
 
-        String username = usernameField.getText().toString();
-        String password = passwordField.getText().toString();
-        String firstName = firstNameField.getText().toString();
-        String lastName = lastNameField.getText().toString();
-        String email = emailField.getText().toString();
-
-        if (!username.matches("[A-za-z0-9]+")) {
+        String username = details.get("username");
+        if (!username.matches("[A-Za-z0-9_]+")) {
             text += "Username must be alphanumeric characters only. \n";
             valid = false;
         }
 
+        String password = details.get("password");
         if (password.length() < 6) {
             text += "Password must be greater than 6 characters. \n";
             valid = false;
         }
 
+        String email = details.get("email");
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             text += "Email is not valid. \n";
             valid = false;
         }
 
+        String firstName = details.get("first_name");
         if (firstName.isEmpty()) {
             text += "First name is empty. \n";
             valid = false;
         }
 
+        String lastName = details.get("last_name");
         if (lastName.isEmpty()) {
             text += "Last name is empty.";
             valid = false;
@@ -120,40 +116,32 @@ public class ProfileEditActivity extends AppCompatActivity {
             return true;
         }
         else {
-            Snackbar snackbar = Snackbar.make(
-                    findViewById(android.R.id.content), text, Snackbar.LENGTH_LONG
-            );
-
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(android.R.id.content), text, Snackbar.LENGTH_LONG);
             // Snackbar doesn't show all of the error text if it's > 2 lines, so we make it
             View snackbarView = snackbar.getView();
             TextView snackbarText = snackbarView.findViewById(R.id.snackbar_text);
             snackbarText.setMaxLines(5);
             snackbar.show();
+
             return false;
         }
     }
 
-    private void updateProfile() throws JSONException, UnsupportedEncodingException {
-        String url = Server.profileEditRoute();
-        JSONObject json = new JSONObject();
+    private void updateProfile(HashMap<String, String> details) throws UnsupportedEncodingException {
+        ProgressBar progressBar = findViewById(R.id.progressBar);
 
-        String username = usernameField.getText().toString();
-        String password = passwordField.getText().toString();
-        String firstName = firstNameField.getText().toString();
-        String lastName = lastNameField.getText().toString();
-        String email = emailField.getText().toString();
-
-        json.put("username", username);
-        json.put("password", password);
-        json.put("email", email);
-        json.put("first_name", firstName);
-        json.put("last_name", lastName);
+        JSONObject json = new JSONObject(details);
         StringEntity entity = new StringEntity(json.toString());
 
+        // Have to auth with API using our old username/password
         String storedUsername = User.getUsername(getApplicationContext());
         String storedPassword = User.getPassword(getApplicationContext());
+
         AsyncHttpClient client = new AsyncHttpClient();
         client.setBasicAuth(storedUsername, storedPassword);
+
+        String url = Server.profileEditRoute();
         client.put(getApplicationContext(), url, entity, "application/json", new JsonHttpResponseHandler() {
             @Override
             public void onStart() {
@@ -162,14 +150,12 @@ public class ProfileEditActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject response) {
-                if (response.has("error")) {
-                    try {
-                        String responseString = response.getString("error");
-                        View thisView = findViewById(android.R.id.content);
-                        Snackbar.make(thisView, responseString, Snackbar.LENGTH_LONG).show();
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                try {
+                    String responseString = response.getString("error");
+                    View thisView = findViewById(android.R.id.content);
+                    Snackbar.make(thisView, responseString, Snackbar.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
                 progressBar.setVisibility(View.INVISIBLE);
             }
@@ -179,10 +165,10 @@ public class ProfileEditActivity extends AppCompatActivity {
                 // Update the User object
                 User.setDetails(
                         getApplicationContext(),
-                        usernameField.getText().toString(),
-                        passwordField.getText().toString()
+                        details.get("username"),
+                        details.get("password")
                 );
-
+                
                 // Go back to MainActivity (as Profile is the default fragment) and reload profile data
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
