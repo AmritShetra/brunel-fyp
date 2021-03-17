6b8332c0db460908310faa7518d8316a64f9787f
Amrit Shetra <1719349@brunel.ac.uk>
2021-03-17 16:00:50 +0000

    Fixed entrypoint - echo should be in `else` clause.

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,8 +4,8 @@
 FILE=my_model/
 if [ ! -d "$FILE" ]; then
   python classifier.py
+else
+  echo "Image classifier already exists... moving on."
 fi
-
-echo "Image classifier already exists... moving on."
 
 exec "$@"
